8ccfff64e83e88b43a2a8cad47eabb9453a2eb81
Ashley Stanley <amstanley43@gmail.com>
2019-02-19 22:23:59 -0500

    Added expenses list

--- src/app/expenses-list/expense.ts
+++ src/app/expenses-list/expense.ts
@@ -1,4 +1,4 @@
 export interface Expense {
   amount: number;
-  expenseDescription: string;
+  description: string;
 }
--- src/app/expenses-list/expenses-list.component.ts
+++ src/app/expenses-list/expenses-list.component.ts
@@ -2,18 +2,35 @@ import { Component, OnInit } from '@angular/core';
 
 import { Expense } from './expense';
 
+
 @Component({
   selector: 'app-expenses-list',
   templateUrl: './expenses-list.component.html',
   styleUrls: ['./expenses-list.component.css']
 })
 export class ExpensesListComponent implements OnInit {
-  pageTitle = 'Expense List';
+  pageTitle = 'Expenses';
   expenses: Expense[] = [];
+  expensesTotal: number;
 
   constructor() { }
 
+  getTotal(expenses: Expense[]): number {
+    let total: number;
+    for(let expense of expenses) {
+      this.expensesTotal += expense.amount;
+    }
+    return total;
+  }
+
   ngOnInit() {
+    this.expenses = [
+                     {amount: 30, description: 'groceries'},
+                     {amount: 600, description: 'rent'},
+                     {amount: 200, description: 'car payment'},
+                     {amount: 20, description: 'electric bill'}
+                    ];
+    this.expensesTotal = this.getTotal(this.expenses);
   }
 
 }
